1532e1931a338b013a20a4cfa95aca2b422ecfea
SanidhyaPanwar <SanidhyaPanwar1@gmail.com>
2022-12-07 23:51:03 +0530

    deploy dec 2022

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
             <span>Come Hang out with me!</span>  
             <span>
             <div className="social">
-                <a href="https://twitter.com/sanidhya_panwar"><i className="fab fa-twitter" aria-hidden="true"></i></a>
-                <a href="https://www.instagram.com/sanidhya_panwar/"><i className="fab fa-instagram" aria-hidden="true"></i></a>
-                <a href="https://www.linkedin.com/in/sanidhya-panwar-824504180/"><i className="fab fa-linkedin" aria-hidden="true"></i></a>
-                <a href="https://github.com/SanidhyaPanwar"><i className="fab fa-github" aria-hidden="true"></i></a>
+                <a href="https://twitter.com/sanidhya_panwar"> <i className="fab fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/sanidhya_panwar/"> <i className="fab fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/sanidhya-panwar-824504180/"> <i className="fab fa-linkedin" aria-hidden="true"></i></a>
+                <a href="https://github.com/SanidhyaPanwar"> <i className="fab fa-github" aria-hidden="true"></i></a>
             </div>
         </span>
-        <span>Created By <a>Sanidhya Panwar</a></span><br/>
+        <span>Created By <a>Sanidhya Panwar </a></span><br/>
         <h4><span>Switch to <a href="https://sanidhyaportfolio.herokuapp.com/">Single page version</a></span></h4>
     </footer>
   )
