a1dea9db315acb91d61623773ecf99b48775207a
SanidhyaPanwar <sanidhyapanwar1@gmail.com>
2025-03-29 12:11:52 +0530

    Added browserrouter base and improved routes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,49 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Experience from './components/Experience';
-import Education from './components/Education';
-import Interests from './components/Interest';
-import Skill from './components/Skill';
-import Skills from './components/Skills';
-import Projects from './components/Project';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import OsiFarming from './components/project/OsiFarming';
-import Verizon from './components/project/Verizon';
-import Indrapal from './components/project/Indrapal';
-import SanidhyaInsta from './components/project/SanidhyaInsta';
-import Covid from './components/project/Covid';
-import BreakingBad from './components/project/BreakingBad';
-import {BrowserRouter,Route} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import Interests from "./components/Interest";
+import Skill from "./components/Skill";
+import Skills from "./components/Skills";
+import Projects from "./components/Project";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import OsiFarming from "./components/project/OsiFarming";
+import Verizon from "./components/project/Verizon";
+import Indrapal from "./components/project/Indrapal";
+import SanidhyaInsta from "./components/project/SanidhyaInsta";
+import Covid from "./components/project/Covid";
+import BreakingBad from "./components/project/BreakingBad";
 
 function App() {
   return (
-    <BrowserRouter basename="/sanidhyapanwar.github.io">
-    <Navbar/>
-    <Route exact path="/" >
-      <Home />
-    </Route>
-    <Route path="/home" >
-      <Home />
-    </Route>
-    <Route path="/about">
-      <About />
-    </Route>
-    <Route path="/experience">
-      <Experience />
-    </Route>
-    <Route path="/education">
-      <Education />
-    </Route>
-    <Route path="/interests">
-      <Interests />
-    </Route>
-    <Route exact path="/skill">
-      <Skill />
-    </Route>
-    <Route path="/skills">
-      <Skills />
-    </Route>
-    <Route path="/projects">
-      <Projects />
-    </Route>
-    <Route path="/contact">
-      <Contact />
-    </Route>
-    {/* Project routing start */}
-    <Route path="/project/verizon">
-      <Verizon />
-    </Route>
-    <Route path="/project/osi-farming">
-      <OsiFarming />
-    </Route>
-    <Route path="/project/sanidhya-insta">
-      <SanidhyaInsta />
-    </Route>
-    <Route path="/project/breaking-bad">
-      <BreakingBad />
-    </Route>
-    <Route path="/project/indrapal">
-      <Indrapal />
-    </Route>
-    <Route path="/project/covid">
-      <Covid />
-    </Route>
-    <Footer/>
-  </BrowserRouter>
+    <Router basename="/">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/interests" element={<Interests />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Project Routes */}
+        <Route path="/project/verizon" element={<Verizon />} />
+        <Route path="/project/osi-farming" element={<OsiFarming />} />
+        <Route path="/project/sanidhya-insta" element={<SanidhyaInsta />} />
+        <Route path="/project/breaking-bad" element={<BreakingBad />} />
+        <Route path="/project/indrapal" element={<Indrapal />} />
+        <Route path="/project/covid" element={<Covid />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
